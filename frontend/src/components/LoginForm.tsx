f0df84c3e1b34150795dc17950e2518bedfc80be
kurominarakmak <pisitpat8189@gmail.com>
2025-06-12 15:44:19 +0700

    feat(login): implement eye field to toggle password on/off

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -3,26 +3,34 @@ import React, { useState } from "react";
 import AuthButton from "./AuthButton";
 import { useRouter } from "next/navigation";
 import { signIn } from "@/actions/auth";
+import { Eye, EyeOff } from "lucide-react";
 
 const LoginForm = () => {
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
   const [loading, setLoading] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState(false);
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setLoading(true);
     setError(null);
     const formData = new FormData(event.currentTarget);
-        const result = await signIn(formData);
-        
-        if(result.status === "success") {
-            router.push("/");
-        }else{
-            setError(result.status);
-        }
+    const result = await signIn(formData);
+    
+    if(result.status === "success") {
+      router.push("/");
+    } else {
+      setError(result.status);
+    }
 
     setLoading(false);
   };
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit} className="w-full flex flex-col gap-4">
@@ -35,20 +43,33 @@ const LoginForm = () => {
             placeholder="Email"
             id="Email"
             name="email"
-            className="mt-1 w-full px-4 p-2  h-10 rounded-md border border-gray-200 bg-white text-sm text-gray-700"
+            className="mt-1 w-full px-4 p-2 h-10 rounded-md border border-gray-200 bg-white text-sm text-gray-700"
           />
         </div>
         <div>
           <label className="block text-sm font-medium text-gray-200">
             Password
           </label>
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            id="password"
-            className="mt-1 w-full px-4 p-2  h-10 rounded-md border border-gray-200 bg-white text-sm text-gray-700"
-          />
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              name="password"
+              id="password"
+              className="mt-1 w-full px-4 p-2 h-10 rounded-md border border-gray-200 bg-white text-sm text-gray-700"
+            />
+            <button
+              type="button"
+              onClick={togglePassword}
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none"
+            >
+              {showPassword ? (
+                <EyeOff className="h-5 w-5" />
+              ) : (
+                <Eye className="h-5 w-5" />
+              )}
+            </button>
+          </div>
         </div>
         <div className="mt-4">
           <AuthButton type="login" loading={loading} />
