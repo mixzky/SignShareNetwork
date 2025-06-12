ba05e3af309b029d0661dfb018924afb5ff7a706
kurominarakmak <pisitpat8189@gmail.com>
2025-06-12 14:30:53 +0700

    refactor(country): update CountryPage to handle async params

--- frontend/src/app/country/[id]/page.tsx
+++ frontend/src/app/country/[id]/page.tsx
@@ -2,11 +2,12 @@ import { Suspense } from 'react';
 import CountryContent from './CountryContent';
 import TopMenu from "@/components/TopMenu";
 
-export default function CountryPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function CountryPage(
+  props: {
+    params: Promise<{ id: string }>;
+  }
+) {
+  const params = await props.params;
   return (
     <main className="flex flex-col min-h-screen bg-[#fafafa]">
       <div className="fixed top-0 left-0 w-full z-50 bg-[#0a0e18] h-24 flex items-center">
